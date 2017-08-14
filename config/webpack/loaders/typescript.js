d2708b2d99b58d3329bab9e82c68c606e1ba9e40
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-08-14 15:19:08 +0530

    Add webpacker config

---
+++ config/webpack/loaders/typescript.js
@@ -0,0 +1,9 @@
+module.exports = {
+	loader: 'ts-loader',
+	test: function(path) {
+		let isTest = (path.substr(path.length - 8) == '.spec.ts');
+		let isTypescript = (path.substr(path.length - 3) == '.ts');
+
+		return isTypescript && !isTest;
+	},
+}
