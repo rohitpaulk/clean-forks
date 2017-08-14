0fdf02d8edc4759b86adcd20b293fc990968d5bc
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-08-14 15:53:04 +0530

    Wire-up typescript

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -7,4 +7,9 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+function HelloWorld(str: string) {
+    console.log('Hello World from ' + str);
+}
+
+
+HelloWorld("HEY");
