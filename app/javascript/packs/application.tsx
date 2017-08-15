426a5e710a8de493423b2679da10e7fab71dd301
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-08-15 17:28:01 +0530

    Extract App into component file

--- app/javascript/packs/application.tsx
+++ app/javascript/packs/application.tsx
@@ -1,49 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { FilterList } from "../components/FilterList";
-import { SearchResultList } from "../components/SearchResultList";
-import { GitRepo } from "../models";
-import { API } from "../API";
-
-interface AppState {
-    repos: GitRepo[]
-}
-
-class App extends React.Component<{}, AppState> {
-    API: API
-
-    constructor(props) {
-        super(props);
-
-        this.API = new API("test_url");
-        this.state = {repos: []};
-    }
-
-    componentWillMount() {
-        let app = this;
-        this.API.getRepos().then(function(repos) {
-            console.log("Setting state");
-            app.setState({repos: repos});
-        });
-    }
-
-    render() {
-        return <div>
-            <div className="filter-list-container">
-                <FilterList/>
-            </div>
-
-            <div className="search-info">
-                24 repositories matched
-            </div>
-
-            <div className="search-result-list-container">
-                <SearchResultList repos={this.state.repos}/>
-            </div>
-        </div>;
-    }
-}
+import { App } from "../components/App";
 
 ReactDOM.render(
     <App />,
