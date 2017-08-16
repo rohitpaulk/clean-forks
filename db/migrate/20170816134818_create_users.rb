fd0ea4a75ba4a5312792165aaa04557cc99d50f6
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-08-16 19:21:17 +0530

    Add basic users table

---
+++ db/migrate/20170816134818_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :avatar_url
+      t.string :auth_token
+
+      t.timestamps
+    end
+  end
+end
