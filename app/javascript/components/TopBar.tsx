12170a27b6c8d4f042e512d3624f84c738025531
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-08-16 18:28:41 +0530

    Add ability to toggle dropdown

--- app/javascript/components/TopBar.tsx
+++ app/javascript/components/TopBar.tsx
@@ -5,10 +5,14 @@ interface TopBarProps {
     user: User | null
 }
 
-function Dropdown(props: {username: string}) {
+interface TopBarState {
+    isDropdownOpen: boolean
+}
+
+function Dropdown(props: {heading: string}) {
     return <div className="dropdown-container">
         <div className="dropdown-header">
-            <span>{props.username}</span>
+            <span>{props.heading}</span>
         </div>
         <div className="dropdown-item-group">
             <div className="dropdown-item">
@@ -21,20 +25,42 @@ function Dropdown(props: {username: string}) {
     </div>;
 }
 
-export function TopBar(props: TopBarProps) {
+export class TopBar extends React.Component<TopBarProps, TopBarState> {
     // TODO: Render empty avatar when user is not fetched
-    return <div className="top-bar">
-        <div className="top-bar-inner">
-            <div className="left-group">
-                <span className="logo-img"></span>
-                <span className="logo-txt">Clean Forks</span>
+    constructor(props) {
+        super(props)
+
+        this.state = {isDropdownOpen: false};
+        this.toggleDropdown = this.toggleDropdown.bind(this);
+    }
+
+    toggleDropdown(e) {
+        e.preventDefault();
+        this.setState(prevState => ({
+            isDropdownOpen: !prevState.isDropdownOpen
+        }));
+    }
+
+    render() {
+        return <div className="top-bar">
+            <div className="top-bar-inner">
+                <div className="left-group">
+                    <span className="logo-img"></span>
+                    <span className="logo-txt">Clean Forks</span>
+                </div>
+                {this.props.user &&
+                    <div className="right-group">
+                        <div className="dropdown-toggle" onClick={this.toggleDropdown}>
+                            <img className="avatar-img"
+                                 src={this.props.user.avatarUrl}>
+                            </img>
+                            <span className="arrow-icon"></span>
+                        </div>
+                        {this.state.isDropdownOpen &&
+                            <Dropdown heading={this.props.user.username}/>}
+                    </div>
+                }
             </div>
-            {props.user &&
-                <div className="right-group">
-                    <img className="avatar-img" src={props.user.avatarUrl}></img>
-                    <span className="arrow-icon"></span>
-                    <Dropdown username={props.user.username}/>
-                </div>}
-        </div>
-    </div>;
+        </div>;
+    }
 }
