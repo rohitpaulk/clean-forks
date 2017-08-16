acb338daa8784e08fdbfa1c1e09e84850e05b942
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-08-16 12:27:13 +0530

    Add fake checks to API

--- app/javascript/API.ts
+++ app/javascript/API.ts
@@ -1,24 +1,53 @@
-import { GitRepo } from './models';
+import { GitRepo, OpenPRCheck, UnmergedBranchCheck } from './models';
 
-let fakeRepos = [
-  {
-    id: '1',
-    parentNameWithOwner: "gratipay/gratipay.com",
-    description: "Gratitude? Gratipay! We help companies and others pay for open source.",
-    forkedAt: 1502863016
-  },
-  {
-    id: '2',
-    parentNameWithOwner: "1egoman/backstroke",
-    description: "🏊 A Github bot to keep repository forks up to date with their upstream. ",
-    forkedAt: 1497592616
-  },
-  {
-    id: '3',
-    parentNameWithOwner: "qunitjs/qunit",
-    description: "An easy-to-use JavaScript Unit Testing framework.",
-    forkedAt: 1466056616
-  }
+let allChecksOk = [
+    {
+        type: "open_prs",
+        status: "success",
+        data: { count: 0, items: [] }
+    } as OpenPRCheck, // Why should I specify the type here?
+
+    {
+        type: "unmerged_branches",
+        status: "success",
+        data: { count: 0, items: [] }
+    } as UnmergedBranchCheck // Why should I specify the type here?
+];
+
+let fakeRepos: GitRepo[] = [
+    {
+        id: '1',
+        parentNameWithOwner: "gratipay/gratipay.com",
+        description: "Gratitude? Gratipay! We help companies and others pay for open source.",
+        forkedAt: 1502863016,
+        checks: allChecksOk
+    },
+    {
+        id: '2',
+        parentNameWithOwner: "1egoman/backstroke",
+        description: "🏊 A Github bot to keep repository forks up to date with their upstream. ",
+        forkedAt: 1497592616,
+        checks: allChecksOk
+    },
+    {
+        id: '3',
+        parentNameWithOwner: "qunitjs/qunit",
+        description: "An easy-to-use JavaScript Unit Testing framework.",
+        forkedAt: 1466056616,
+        checks: [
+            {
+                type: "open_prs",
+                status: "success",
+                data: { count: 0, items: [] }
+            } as OpenPRCheck, // Why should I specify the type here?
+
+            {
+                type: "unmerged_branches",
+                status: "failure",
+                data: { count: 1, items: [] } // TODO: Add items
+            } as UnmergedBranchCheck // Why should I specify the type here?
+        ]
+    }
 ];
 
 export class API {
--- app/javascript/models.ts
+++ app/javascript/models.ts
@@ -1,8 +1,39 @@
+export interface OpenPRCheckData {
+    count: number
+    items: {
+        url: string
+        prNumber: string
+        title: string
+    }[]
+}
+
+export interface UnmergedBranchCheckData {
+    count: number
+    items: {
+        name: string
+        url: string
+    }[]
+}
+
+export interface OpenPRCheck {
+    type: "open_prs"
+    status: "pending" | "success" | "failure"
+    data: OpenPRCheckData
+}
+
+export interface UnmergedBranchCheck {
+    type: "unmerged_branches"
+    status: "pending" | "success" | "failure"
+    data: UnmergedBranchCheckData
+}
+
+export type GitRepoCheck = OpenPRCheck | UnmergedBranchCheck;
+
 export interface GitRepo {
     id: string
     parentNameWithOwner: string
     description: string
     forkedAt: number // Unix timestamp
 
-    // TODO: Add check status
+    checks: GitRepoCheck[]
 }
