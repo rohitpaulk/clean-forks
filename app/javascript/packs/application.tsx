6e50d7fd4abdbb7217c396b52464f43e0a0b643c
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-10-11 14:47:16 +0530

    Enable fake API under test query string

--- app/javascript/packs/application.tsx
+++ app/javascript/packs/application.tsx
@@ -3,8 +3,13 @@ import * as ReactDOM from "react-dom";
 
 import { App } from "../components/App";
 import { API } from "../API";
+import { FakeBootAPI } from "../FakeBootAPI";
 
 let apiUrl = document.head.querySelector("[property=apiUrl]")!["content"];
 let api = new API(apiUrl);
+
+if (window.location.search === "?test") {
+    api = new FakeBootAPI("dummy_url");
+}
 
 ReactDOM.render(<App api={api} />, document.getElementById("app"));
