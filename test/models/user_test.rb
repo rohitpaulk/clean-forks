db51a3712e507fe21f5132345eb99edba5002c37
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-10-12 04:38:37 +0530

    s/to_json/as_json/g

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,23 +17,23 @@ class UserTest < ActiveSupport::TestCase
     assert_equal(2, user.git_repositories.count)
   end
 
-  test "#to_json returns proper resource" do
+  test "#as_json returns proper resource" do
     user = FactoryGirl.create(:user)
-    json = user.to_json
+    json = user.as_json
 
     assert_equal(user.username, json[:username])
   end
 
-  test "#to_json returns 0 if synced_at is nil" do
+  test "#as_json returns 0 if synced_at is nil" do
     user = FactoryGirl.create(:user, git_repositories_synced_at: nil)
-    json = user.to_json
+    json = user.as_json
 
     assert_equal(0, json[:git_repositories_synced_at])
   end
 
-  test "#to_json returns > 0 if synced_at is nil" do
+  test "#as_json returns > 0 if synced_at is nil" do
     user = FactoryGirl.create(:user, git_repositories_synced_at: Time.now)
-    json = user.to_json
+    json = user.as_json
 
     assert(json[:git_repositories_synced_at] > 0)
   end
